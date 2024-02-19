4744767e354324c6281e86a6f41674b4b971b52c
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-02-18 19:49:00 -0500

    Asiento

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -2,4 +2,14 @@ package test;
 
 public class Asiento {
     
-}
+    String color;
+    int precio;
+    int registro;
+
+    void cambioColor(String color){
+        if (color == "rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco"){
+            this.color = color;
+        }
+
+    }
+    }
