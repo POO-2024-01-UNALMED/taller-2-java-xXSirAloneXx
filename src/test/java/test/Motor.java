d9b224802f3036d4e7f0228aacd42d5d5822eb38
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-02-19 15:54:04 -0500

    1

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -5,10 +5,12 @@ public class Motor {
     String tipo;
     int registro;
 
-    void cambiarRegistro(int registro){
+    void cambiarRegistro(int registro)
+    {
         this.registro = registro;
     }
-    void asignarTipo(String tipo){
+    void asignarTipo(String tipo)
+    {
         if (tipo == "electrico" || tipo == "gasolina")
         {
             this.tipo = tipo;
