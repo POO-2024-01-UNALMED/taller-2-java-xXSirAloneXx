036d62d9a628b67a47989f5c7e1ed5c2150f4fdf
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-02-18 21:01:15 -0500

    Auto

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,5 +1,16 @@
 package test;
 
 public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+
+    void cambiarRegistro(int registro){
+
+    }
+    void asignarTipo(String tipo){
+        
+    }
+
     
 }
